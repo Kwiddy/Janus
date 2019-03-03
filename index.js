23a8984c25d962a1f29fd078b247a9cd30e3087f
Kwiddy <44179795+Kwiddy@users.noreply.github.com>
2019-03-03 15:27:39 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 function toggle_div(id) {
   var elem = document.getElementById(id);
 
-  console.log(elem);
-
-  if (elem.style.display == 'none') {
+  if (document.getElementById("toggleAdd").innerHTML == "Hide") {
+    elem.style.display = 'none';
+    document.getElementById("toggleAdd").innerHTML = "Create Your Own Event!";
+  }
+  else {
     elem.style.display = 'block';
     document.getElementById("toggleAdd").innerHTML = "Hide";
   }
-  else {
-    elem.style.display = 'none';
-    document.getElementById("toggleAdd").innerHTML = "Add Event";
-  }
+}
+
+function CreateEvent() {
+  var newEvent = document.createElement("P");
+  var content = document.createTextNode(document.getElementById("host").value);
+
+  newEvent.appendChild(content);
+  document.body.appendChild(newEvent)
+  newEvent.className = "CreatedEvent"
 }
