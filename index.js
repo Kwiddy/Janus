a0474026b44406c822615198a6181d8860f984b7
Kwiddy <44179795+Kwiddy@users.noreply.github.com>
2019-03-05 20:11:35 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
-function toggle_div(id) {
-  var elem = document.getElementById(id);
+function toggle_hidden(div) {
+  var elem = document.getElementById(div);
 
-  if (document.getElementById("toggleAdd").innerHTML == "Hide") {
+  if (document.getElementById("newJob").innerHTML == "Cancel") {
     elem.style.display = 'none';
-    document.getElementById("toggleAdd").innerHTML = "Create Your Own Event!";
+    document.getElementById("newJob").innerHTML = "Post a Job";
   }
   else {
     elem.style.display = 'block';
-    document.getElementById("toggleAdd").innerHTML = "Hide";
+    document.getElementById("newJob").innerHTML = "Cancel";
   }
-}
-
-function CreateEvent() {
-  var newEvent = document.createElement("P");
-  var content = document.createTextNode(document.getElementById("host").value);
-
-  newEvent.appendChild(content);
-  document.body.appendChild(newEvent)
-  newEvent.className = "CreatedEvent"
 }
