e0a4235ab23ab314645df66438c2775a62d18138
Kwiddy <44179795+Kwiddy@users.noreply.github.com>
2019-03-08 15:45:24 +0000

    Added onStart()

--- client/index.js
+++ client/index.js
@@ -1,3 +1,14 @@
+
+function onStart() {
+  var addEmpl;
+  var addJob;
+  $("#submit").click(function() {
+    addEmpl = $("#addEmpl").val();
+    addJob = $("#addJob").val();
+    $.post("http://127.0.0.1:8090/index", {addEmpl:addEmpl , addJob:addJob});
+  });
+}
+
 function toggle_hidden(div) {
   var elem = document.getElementById(div);
 
