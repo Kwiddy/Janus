1c51803d3c9d7c16182fc9f6bf58bb5bb6965be9
Kwiddy <44179795+Kwiddy@users.noreply.github.com>
2019-03-12 11:54:14 +0000

    Added Job response getting

--- server.js
+++ server.js
@@ -2,23 +2,29 @@ var express = require("express");
 var myParser = require("body-parser");
 var app = express();
 
+let jobs = [
+  "dummy",
+  "dummy 2"
+];
+
 app.use(myParser.urlencoded({extended:false}));
 app.use(myParser.json());
 app.use(express.static("client"));
 
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + "/client/index.html");
+app.get('/jobsList', function(request, response) {
+  response.send(jobs);
 });
 
-app.post('/index', function(request, response) {
+app.post('/add', function(request, response) {
   var employer = request.body.addEmpl;
   var jobTitle = request.body.addJob;
   console.log("Employer added: " + employer + ", offering the position of: " + jobTitle);
+
+  ////////////Needs to be included to add both eventually
+  jobs.push(employer);
+
   response.end("yes");
 });
-
-
-
 
 app.listen(8090, function() {
   console.log("SERVER RUNNING ON PORT 8090");
