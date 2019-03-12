a431931e88001028a3306e4073481aefbd32adc7
Kwiddy <44179795+Kwiddy@users.noreply.github.com>
2019-03-12 15:29:37 +0000

    Update index.js

--- client/index.js
+++ client/index.js
@@ -26,27 +26,49 @@ function toggle_hidden(div) {
 window.addEventListener('click', async function(event){
   let response = await fetch('http://127.0.0.1:8090/jobsList')
   let body = await response.text();
-  let jobs = JSON.parse(body);
+  let employersList = JSON.parse(body);
 
   document.getElementById("postedJobs").innerHTML = "<ul>";
 
-  for(let i = 0; i < jobs.length; i++) {
-    document.getElementById("postedJobs").innerHTML += "<li class='jobsEntries'>" + jobs[i] + "</li>";
-  }
+  for(let i = 0; i < employersList.length; i++) {
+    document.getElementById("postedJobs").innerHTML += "<li class='noStyle'> <div class='jobsEntries'>" + employersList[i] + "</div> </li>";
+  } //SEMICOLON
 
   document.getElementById("postedJobs").innerHTML += "</ul>";
+
 });
 
 window.addEventListener('load', async function(event){
   let response = await fetch('http://127.0.0.1:8090/jobsList')
   let body = await response.text();
-  let jobs = JSON.parse(body);
+  let employersList = JSON.parse(body);
 
   document.getElementById("postedJobs").innerHTML = "<ul>";
 
-  for(let i = 0; i < jobs.length; i++) {
-    document.getElementById("postedJobs").innerHTML += "<li class='jobsEntries'>" + jobs[i] + "</li>";
-  }
+  for(let i = 0; i < employersList.length; i++) {
+    document.getElementById("postedJobs").innerHTML += "<li class='noStyle'> <div class='jobsEntries'>" + employersList[i] + "</div> </li>";
+  } //SEMICOLON
 
   document.getElementById("postedJobs").innerHTML += "</ul>";
 });
+
+
+
+
+
+
+
+
+
+/*
+let response = await fetch('http://127.0.0.1:8090/jobsList')
+let body = await response.text();
+let employersList = JSON.parse(body);
+
+for(let i = 0; i < employersList.length; i++) {
+  var newPost = document.createElement("div");
+  newPost.innerHTML = employersList[i];
+  newPost.className = "jobsEntries";
+  document.body.appendChild(newPost);
+};
+*/
