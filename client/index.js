ec9a45ddf0e1564c5f28d5d7c7d844d089e30e30
Kwiddy <44179795+Kwiddy@users.noreply.github.com>
2019-03-17 15:22:02 +0000

    Fixed Lag bug and Added submit hidden toggle

--- client/index.js
+++ client/index.js
@@ -2,11 +2,34 @@
 function onStart() {
   var addEmpl;
   var addJob;
-  $("#submit").click(function() {
+  var addDesc;
+  $("#submit").click(async function() {
+
+    toggle_hidden('postJob');
+
     addEmpl = $("#addEmpl").val();
     addJob = $("#addJob").val();
     addDesc = $("#addDesc").val();
     $.post("http://127.0.0.1:8090/add", {addEmpl:addEmpl , addJob:addJob, addDesc:addDesc});
+
+    let response = await fetch('http://127.0.0.1:8090/empList')
+    let descresp = await fetch('http://127.0.0.1:8090/descList')
+    let jobresp = await fetch('http://127.0.0.1:8090/jobList')
+    let body = await response.text();
+    let descbody = await descresp.text();
+    let jobbody = await jobresp.text();
+    let empList = JSON.parse(body);
+    let descriptionsList = JSON.parse(descbody);
+    let jobList = JSON.parse(jobbody);
+
+    document.getElementById("postedJobs").innerHTML = "<div>";
+
+    for(let i = 0; i < empList.length; i++) {
+        document.getElementById("postedJobs").innerHTML += "<div class='jobsEntries'> <b> " + empList[i] + "</b> - " + jobList[i] + "<br> <span>" + descriptionsList[i] + "</span></div>";
+    };
+
+    document.getElementById("postedJobs").innerHTML += "</div>";
+
   });
 }
 
@@ -22,40 +45,21 @@ function toggle_hidden(div) {
   }
 }
 
-window.addEventListener('click', async function(event){
-
-  let response = await fetch('http://127.0.0.1:8090/jobsList')
-  let descresp = await fetch('http://127.0.0.1:8090/descList')
-  let body = await response.text();
-  let descbody = await descresp.text();
-  let submittedList = JSON.parse(body);
-  let descriptionsList = JSON.parse(descbody);
-
-  document.getElementById("postedJobs").innerHTML = "<div>";
-
-  for(let i = 0; i < submittedList.length-1; i++) {
-    if (i % 2 == 0) {
-      document.getElementById("postedJobs").innerHTML += "<div class='jobsEntries'> <b> " + submittedList[i] + "</b> - " + submittedList[i+1] + "<br>" + descriptionsList[i] + "</div>";;
-    }
-  };
-
-  document.getElementById("postedJobs").innerHTML += "</div>";
-});
-
 window.addEventListener('load', async function(event){
-  let response = await fetch('http://127.0.0.1:8090/jobsList')
+  let response = await fetch('http://127.0.0.1:8090/empList')
   let descresp = await fetch('http://127.0.0.1:8090/descList')
+  let jobresp = await fetch('http://127.0.0.1:8090/jobList')
   let body = await response.text();
   let descbody = await descresp.text();
-  let submittedList = JSON.parse(body);
+  let jobbody = await jobresp.text();
+  let empList = JSON.parse(body);
   let descriptionsList = JSON.parse(descbody);
+  let jobList = JSON.parse(body);
 
   document.getElementById("postedJobs").innerHTML = "<div>";
 
-  for(let i = 0; i < submittedList.length-1; i++) {
-    if (i % 2 == 0) {
-      document.getElementById("postedJobs").innerHTML += "<div class='jobsEntries'> <b> " + submittedList[i] + "</b> - " + submittedList[i+1] + "<br>" + descriptionsList[i] + "</div>";
-    }
+  for(let i = 0; i < empList.length; i++) {
+      document.getElementById("postedJobs").innerHTML += "<div class='jobsEntries'> <b> " + empList[i] + "</b> - " + jobList[i] + "<br> <span>" + descriptionsList[i] + "</span></div>";
   };
 
   document.getElementById("postedJobs").innerHTML += "</div>";
