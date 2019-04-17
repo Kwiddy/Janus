bee2467fb2a65be12458533517977d5b155bd93e
Kwiddy <44179795+Kwiddy@users.noreply.github.com>
2019-04-17 13:57:22 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require("express");
 var myParser = require("body-parser");
 var app = express();
 
+
+
 let empList = [];
 let descriptions = [];
 let jobList = [];
@@ -12,32 +14,32 @@ app.use(myParser.urlencoded({extended:false}));
 app.use(myParser.json());
 app.use(express.static("client"));
 
-app.get('/empList', function(request, response) {
-  response.send(empList);
+app.get('/empList', function(req, res) {
+  res.send(empList);
 });
 
-app.get('/jobList', function(request, response) {
-  response.send(jobList);
+app.get('/jobList', function(req, res) {
+  res.send(jobList);
 });
 
-app.get('/descList', function(request, response) {
-  response.send(descriptions);
+app.get('/descList', function(req, res) {
+  res.send(descriptions);
 });
 
-app.get('/linkList', function(request, response) {
-  response.send(linkList);
+app.get('/linkList', function(req, res) {
+  res.send(linkList);
 })
 
-app.get('/imgList', function(request, response) {
-  response.send(imgList);
+app.get('/imgList', function(req, res) {
+  res.send(imgList);
 })
 
-app.post('/add', function(request, response) {
-  var employer = request.body.addEmpl;
-  var jobTitle = request.body.addJob;
-  var desc = request.body.addDesc;
-  var link = request.body.addURL;
-  var img = request.body.addImg;
+app.post('/add', function(req, res) {
+  var employer = req.body.addEmpl;
+  var jobTitle = req.body.addJob;
+  var desc = req.body.addDesc;
+  var link = req.body.addURL;
+  var img = req.body.addImg;
   console.log("Employer: " + employer + ", Title: " + jobTitle + ", Description: " + desc + ", URL: " + link + ", Image: " + img);
 
   empList.push(employer);
@@ -46,9 +48,11 @@ app.post('/add', function(request, response) {
   linkList.push(link);
   imgList.push(img)
 
-  response.end("yes");
+  res.end("yes");
 });
 
 app.listen(8090, function() {
   console.log("SERVER RUNNING ON PORT 8090");
 });
+
+module.exports = app;
