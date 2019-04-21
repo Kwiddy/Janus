0587a3a5c323c61eebb67bc2345597af68650bae
Kwiddy <44179795+Kwiddy@users.noreply.github.com>
2019-04-21 19:53:20 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,29 @@
 const app =  require('./app');
-const express = require('express')
+const express = require('express');
+const uuid = require('uuid/v4');
+const session = require('express-session');
+
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "https://janusjobs.herokuapp.com/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+
+app.get('/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
 
 app.listen(process.env.PORT||8090);
