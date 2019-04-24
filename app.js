bb145ccd50fb8e4b9706b81e819a48fcd0e75360
Kwiddy <44179795+Kwiddy@users.noreply.github.com>
2019-04-24 12:34:18 +0100

    Update app.js

--- app.js
+++ app.js
@@ -71,9 +71,12 @@ passport.deserializeUser(function (obj, done) {
 });
 
 passport.use(new GoogleStrategy({
-    clientID: 1042353776096-b40nc822i1clrtc12gc7tiu3g57hin85.apps.googleusercontent.com,
-    clientSecret: _2gzvWyy4Mt_FK6c3KyzAzex,
-    callbackURL: "https://janusjobs.herokuapp.com/auth/google/callback"
+    //clientID: 1042353776096-b40nc822i1clrtc12gc7tiu3g57hin85.apps.googleusercontent.com,
+    //clientSecret: _2gzvWyy4Mt_FK6c3KyzAzex,
+    //callbackURL: "https://janusjobs.herokuapp.com/auth/google/callback"
+    clientID: client_id,
+    clientSecret: client_secret,
+    callbackURL: redirect_uri
   },
   function(accessToken, refreshToken, profile, expires_in, done) {
     return done(null, {user:profile, accessToken:accessToken, refreshToken:refreshToken});
