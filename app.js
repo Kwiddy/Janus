5e79b38030110096fbfae4045c8e2e9786e72d9a
Kwiddy <44179795+Kwiddy@users.noreply.github.com>
2019-04-29 15:56:02 +0100

    Added jsdoc config and regular comments

--- app.js
+++ app.js
@@ -1,41 +1,79 @@
+//Initial Setup
 var express = require("express");
 var myParser = require("body-parser");
 var app = express();
 
+app.use(myParser.urlencoded({extended:false}));
+app.use(myParser.json());
+app.use(express.static("client"));
+
+//Creation of Entity Lists
 let empList = [];
 let descriptions = [];
 let jobList = [];
 let linkList = [];
 let imgList = [];
 
-app.use(myParser.urlencoded({extended:false}));
-app.use(myParser.json());
-app.use(express.static("client"));
-
+/** Attempts a General GET request from the Webpage
+  * @name GET /
+  * @path {GET} /
+  * @code {200} if response is successful
+*/
 app.get("/", (req, res) => {
 	res.status(200).send("/ received");
 });
 
+/** Attempts a GET request to the list of employers from the Webpage
+  * @name GET /empList
+  * @path {GET} /empList
+  * @code {200} if response is successful
+*/
 app.get("/empList", function(req, res) {
 	res.status(200).send(empList);
 });
 
+/** Attempts a GET request to the list of job titles from the Webpage
+  * @name GET /jobList
+  * @path {GET} /jobList
+  * @code {200} if response is successful
+*/
 app.get("/jobList", function(req, res) {
 	res.status(200).send(jobList);
 });
 
+/** Attempts a GET request to the list of job descriptions from the Webpage
+  * @name GET /descList
+  * @path {GET} /descList
+  * @code {200} if response is successful
+*/
 app.get("/descList", function(req, res) {
 	res.status(200).send(descriptions);
 });
 
+/** Attempts a GET request for the list of links to jobs from the Webpage
+  * @name GET /linkList
+  * @path {GET} /linkList
+  * @code {200} if response is successful
+*/
 app.get("/linkList", function(req, res) {
 	res.status(200).send(linkList);
 });
 
+/** Attempts a GET request for the list of profile images from the Webpage
+  * @name GET /imgList
+  * @path {GET} /imgList
+  * @code {200} if response is successful
+*/
 app.get("/imgList", function(req, res) {
 	res.status(200).send(imgList);
 });
 
+/** Attempts a POST to the webpage, this post sends all entities:
+  * employer, job title, job description, job link, and profile image.
+  * @name POST /add
+  * @path {POST} /add
+  * @code {200} if sending is successful
+*/
 app.post("/add", function(req, res) {
 	var employer = req.body.addEmpl;
 	var jobTitle = req.body.addJob;
@@ -49,14 +87,17 @@ app.post("/add", function(req, res) {
 	linkList.push(link);
 	imgList.push(img);
 
+	res.status(200);
 	res.end("End");
 });
 
 ///////////////////////////////
-var passport = require("passport");
 
+//Further Setup
+var passport = require("passport");
 var GoogleStrategy = require("passport-google-oauth20").Strategy;
 
+//User sessions control via Serialize and Deserialize
 passport.serializeUser(function (user, done) {
 	done(null, user);
 });
@@ -64,27 +105,37 @@ passport.deserializeUser(function (obj, done) {
 	done(null, obj);
 });
 
+//Session control with ClientID and clientSecret
 passport.use(new GoogleStrategy({
 	clientID: "1042353776096-b40nc822i1clrtc12gc7tiu3g57hin85.apps.googleusercontent.com",
 	clientSecret: "_2gzvWyy4Mt_FK6c3KyzAzex",
 	callbackURL: "https://janusjobs.herokuapp.com/auth/google/callback"
-	//callbackURL: "localhost:8090"
+	//callbackURL: "localhost:8090" //for use with local testing
 },
 function(accessToken, refreshToken, profile, done) {
 	return done(null, {user:profile, accessToken:accessToken, refreshToken:refreshToken});
 }
 ));
 
+//Initialising Passport Session
 app.use(passport.initialize());
 app.use(passport.session());
 
+/** Attempts a GET request from GOOGLE OAUTH API through passport
+  * @name GET /auth/google
+  * @path {GET} /auth/google
+*/
 app.get("/auth/google",
 	passport.authenticate("google", { scope: ["profile"] }));
 
+/** Attempts a GET request from GOOGLE OAUTH API Callback through passport
+  * @name GET /auth/google/callback
+  * @path {GET} /auth/google/callback
+*/
 app.get("/auth/google/callback",
 	passport.authenticate("google", { failureRedirect: "/" }),
 	function(req, res) {
-		res.redirect("/");
+		res.redirect("/"); //redirecting page
 	});
 
 /////////////////////
