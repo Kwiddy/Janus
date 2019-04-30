db58b424119eab7f0f5086739a69b0b302b175a2
Kwiddy <44179795+Kwiddy@users.noreply.github.com>
2019-04-30 21:54:17 +0100

    big update, eslint compatible

--- app.test.js
+++ app.test.js
@@ -1,126 +1,148 @@
-const app = require('./app');
-const request = require('supertest');
+const app = require("./app");
+const request = require("supertest");
 
-describe('Test the various entities', () => {
+describe("Test the various entities", () => {
 
-  //Testing requests succeed
+	//Testing requests succeed
 
-  test('Test response for GET method', () => {
-    return request(app).get('/').expect(200);
-  });
+	test("Test response for GET method", () => {
+		return request(app).get("/").expect(200);
+	});
 
-  test('GET /empList succeeds', (done) => {
-    return request(app)
-      .get('/empList')
-      .expect(200)
-      .end(function (err) {
-        if (err) return done(err);
-        done();
-      });
-  });
+	test("GET /empList succeeds", (done) => {
+		return request(app)
+			.get("/empList")
+			.expect(200)
+			.end(function (err) {
+				if (err) return done(err);
+				done();
+			});
+	});
 
-  test('GET /jobList succeeds', (done) => {
-    return request(app)
-      .get('/jobList')
-      .expect(200)
-      .end(function (err) {
-        if (err) return done(err);
-        done();
-      });
-  });
+	test("GET /jobList succeeds", (done) => {
+		return request(app)
+			.get("/jobList")
+			.expect(200)
+			.end(function (err) {
+				if (err) return done(err);
+				done();
+			});
+	});
 
-  test('GET /descList succeeds', (done) => {
-    return request(app)
-      .get('/descList')
-      .expect(200)
-      .end(function (err) {
-        if (err) return done(err);
-        done();
-      });
-  });
+	test("GET /descList succeeds", (done) => {
+		return request(app)
+			.get("/descList")
+			.expect(200)
+			.end(function (err) {
+				if (err) return done(err);
+				done();
+			});
+	});
 
-  test('GET /linkList succeeds', (done) => {
-    return request(app)
-      .get('/linkList')
-      .expect(200)
-      .end(function (err) {
-        if (err) return done(err);
-        done();
-      });
-  });
+	test("GET /linkList succeeds", (done) => {
+		return request(app)
+			.get("/linkList")
+			.expect(200)
+			.end(function (err) {
+				if (err) return done(err);
+				done();
+			});
+	});
 
-  test('GET /imgList succeeds', (done) => {
-    return request(app)
-      .get('/imgList')
-      .expect(200)
-      .end(function (err) {
-        if (err) return done(err);
-        done();
-      });
-  });
+	test("GET /imgList succeeds", (done) => {
+		return request(app)
+			.get("/imgList")
+			.expect(200)
+			.end(function (err) {
+				if (err) return done(err);
+				done();
+			});
+	});
 
-  //Testing response types
+	test("GET /auth/google", (done) => {
+		return request(app)
+			.get("/auth/google")
+			.expect(302)
+			.end(function (err) {
+				if (err) return done(err);
+				done();
+			});
+	});
 
-  test('GET /empList returns JSON', (done) => {
-    return request(app)
-	    .get('/empList')
-	    .expect('Content-type', /json/)
-      .end(function (err) {
-        if (err) return done(err);
-        done();
-      });
-    });
+	//Testing response types
 
-  test('GET /jobList returns JSON', (done) => {
-    return request(app)
-	    .get('/jobList')
-	    .expect('Content-type', /json/)
-      .end(function (err) {
-        if (err) return done(err);
-        done();
-      });
-    });
+	test("GET /empList returns JSON", (done) => {
+		return request(app)
+			.get("/empList")
+			.expect("Content-type", /json/)
+			.end(function (err) {
+				if (err) return done(err);
+				done();
+			});
+	});
 
-  test('GET /descList returns JSON', (done) => {
-    return request(app)
-	    .get('/descList')
-	    .expect('Content-type', /json/)
-      .end(function (err) {
-        if (err) return done(err);
-        done();
-      });
-    });
+	test("GET /jobList returns JSON", (done) => {
+		return request(app)
+			.get("/jobList")
+			.expect("Content-type", /json/)
+			.end(function (err) {
+				if (err) return done(err);
+				done();
+			});
+	});
 
-  test('GET /linkList returns JSON', (done) => {
-    return request(app)
-	    .get('/linkList')
-	    .expect('Content-type', /json/)
-      .end(function (err) {
-        if (err) return done(err);
-        done();
-      });
-    });
+	test("GET /descList returns JSON", (done) => {
+		return request(app)
+			.get("/descList")
+			.expect("Content-type", /json/)
+			.end(function (err) {
+				if (err) return done(err);
+				done();
+			});
+	});
 
-  test('GET /imgList returns JSON', (done) => {
-    return request(app)
-	    .get('/imgList')
-	    .expect('Content-type', /json/)
-      .end(function (err) {
-        if (err) return done(err);
-        done();
-      });
-    });
+	test("GET /linkList returns JSON", (done) => {
+		return request(app)
+			.get("/linkList")
+			.expect("Content-type", /json/)
+			.end(function (err) {
+				if (err) return done(err);
+				done();
+			});
+	});
 
-  //Testing POSTs
+	test("GET /imgList returns JSON", (done) => {
+		return request(app)
+			.get("/imgList")
+			.expect("Content-type", /json/)
+			.end(function (err) {
+				if (err) return done(err);
+				done();
+			});
+	});
 
-  test('POST /add succeeds', (done) => {
-    return request(app)
-      .post('/add')
-      .expect(200)
-      .end(function (err) {
-        if (err) return done(err);
-        done();
-      });
-  });
+	//Testing POSTs
+
+	test("POST /add succeeds", (done) => {
+		return request(app)
+			.post("/add")
+			.expect(200)
+			.end(function (err) {
+				if (err) return done(err);
+				done();
+			});
+	});
+
+	//Testing redirect
+
+	test("REDIRECT Google OAUTH", (done) => {
+		return request(app)
+			.get("/auth/google/callback")
+			.expect(302)
+			.end(function (err) {
+				if (err) return done(err);
+				done();
+			});
+	});
 
 });
